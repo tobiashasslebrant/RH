73fc2753dabea3ab1a6b2952dd59a1aab3d3a461
Tobias Hasslebrant <tobias.hasslebrant@gmail.com>
2017-01-03 13:43:40 +0100

    ajax calls

--- scripts/main.js
+++ scripts/main.js
@@ -1,10 +1,23 @@
-var app4 = new Vue({
-  el: '#app-4',
-  data: {
-    todos: [
-      { text: 'Learn JavaScript' },
-      { text: 'Learn Vue' },
-      { text: 'Build something awesome' }
-    ]
+var VanillaAjax = function(){
+  return {
+    get:  (callback) => {
+      var xhr = new XMLHttpRequest();
+      xhr.open('GET', 'api/matches');
+      xhr.onload = () => {
+          if (xhr.status === 200) {
+              callback(xhr.responseText);
+          }
+      };
+      xhr.send();
+    }
   }
-})
+}();
+
+VanillaAjax.get(data => {
+  new Vue({
+    el: '#app-4',
+    data: {
+      matches: JSON.parse(data)
+    }
+  })
+});
